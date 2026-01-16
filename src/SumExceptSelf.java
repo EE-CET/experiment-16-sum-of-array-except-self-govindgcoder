6ae817ee3f42226aed1a5aa04ad679e9ae408c31
govind <govind.g.coder@gmail.com>
2026-01-16 07:47:50 +0530

    initail commit

--- src/SumExceptSelf.java
+++ src/SumExceptSelf.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class SumExceptSelf {
    
         // TODO: Read n
@@ -5,4 +7,22 @@ public class SumExceptSelf {
         // TODO: Calculate sum of all elements (Total Sum)
         // TODO: For each element, print (Total Sum - element)
     
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int nums[] = new int[n];
+        // int output[] = new int[n]; this array is unnecessary since we just needs to print the valeu representing the arrays
+        int sum = 0;
+        // we can calculate the sum in the same loop too, thus reducing the complxity
+        for (int i = 0; i < n;i++) {
+            nums[i] = sc.nextInt();
+            sum+=nums[i];
+        }
+        for (int j =0; j<n; j++) {
+            // output[j]=sum-nums[j];
+            System.out.print(sum-nums[j]);
+            if(j!=n-1) System.out.print(" ");
+        }
+        
+    }
 }
